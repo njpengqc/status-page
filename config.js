479e9a9a3f596617d2b866443e45f882b70dab81
nanjingpqc <87556536+njpengqc@users.noreply.github.com>
2023-08-27 16:28:53 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'nanjingpqc旗下站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1997037-1835a380879fdf3986ee6f50',
+  ],
+
+  // 日志天数
+  CountDays: 10,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://dora-channel.ml'
+    },
+    {
+      text: '视频站',
+      url: 'https://video.dora-channel.ml'
+    },
+    {
+      text: '网盘',
+      url: 'https://alist.dora-channel.ml'
+    },
+    {
+      text: '哆啦盘',
+      url: 'https://pan.nanjingpqc.repl.co/'
+    },
+     {
+      text: 'Minecraft服务器状态检测网站',
+      url: 'https://mc.dora-channel.ml'
+    },
+  ],
+};
